f56ccb044dc61219660bad7bf8a34a486bdddcfb
Aurer Vedran <verkauf15@intern.highlife.at>
2022-11-23 14:29:33 +0100

    results

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 
+import reiseFinden from './reiseFinden';
 function App() {
   return (
     <div className="App">
---
+++ src/reiseFinden.js
@@ -0,0 +1,10 @@
+class reiseFinden extends Component {
+    render() {
+        return (
+            <div>
+                <div id="hlrAppBooking"></div>
+            </div>
+        );
+    }
+}
+export default reiseFinden;
